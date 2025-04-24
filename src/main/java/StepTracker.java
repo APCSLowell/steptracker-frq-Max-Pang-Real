c0ebb6576aba9a314327076fc96f4db4551b0fe0
Max-Pang-Real <mapang@s.sfusd.edu>
2025-04-24 12:27:00 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,23 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int minSteps, totalDays, activeTotal, totalSteps;
+ public StepTracker(int goal){
+   minSteps = goal;
+   totalDays = 0;
+   totalSteps = 0;
+   activeTotal = 0;
+ }
+ public void addDailySteps(int steps){
+   totalSteps+=steps;
+   totalDays++;
+   if(steps >= minSteps)
+    activeTotal++;
+ }
+ public int activeDays(){
+   return activeTotal;
+ }
+ public int averageSteps(){
+  return (double)totalSteps/totalDays;
+ }
 } 
